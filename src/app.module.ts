453644672a3d3a8d353a82cf572cf4b77cf3d282
iago_abner <iagoabner.dev@gmail.com>
2023-01-10 20:34:36 -0300

    feat: config typeOrmModule and add env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './app/todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get('DATABASE_HOST', 'localhost'),
+        port: Number(configService.get('DATABASE_PORT', 3306)),
+        username: configService.get('DATABASE_USER', 'root'),
+        password: configService.get('DATABASE_PASSWORD', '123'),
+        database: configService.get('DATABASE_TABLE', 'todo'),
+        entities: [],
+        synchronize: true,
+        // synchronize: true -> shouldn't be used in production
+      }),
+    }),
+    TodoModule,
+  ],
   controllers: [],
   providers: [],
 })
