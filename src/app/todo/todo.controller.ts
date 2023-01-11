49d9280bd9759ea2835270ac222471580433f9e9
iago_abner <iagoabner.dev@gmail.com>
2023-01-11 20:57:13 -0300

    feat: import todoServices and implementing on routes

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
 
-@Controller('api/todo')
-export class TodoController {}
+@Controller('api/todos')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get(':id')
+  async find(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.todoService.findOne(id);
+  }
+
+  @Get()
+  async list() {
+    return await this.todoService.findAll();
+  }
+
+  @Post()
+  async create(@Body() body) {
+    return await this.todoService.create(body);
+  }
+
+  @Put(':id')
+  async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body) {
+    return await this.todoService.update(id, body);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.todoService.delete(id);
+  }
+}
